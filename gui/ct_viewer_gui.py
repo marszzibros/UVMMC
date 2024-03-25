7142cd17725d79bb8d6f4a85a04bd033483260eb
marszzibros <74686781+marszzibros@users.noreply.github.com>
2024-03-25 16:20:19 -0400

    Update ct_viewer_gui.py

--- gui/ct_viewer_gui.py
+++ gui/ct_viewer_gui.py
@@ -60,7 +60,7 @@ def random_start():
 
     plt.at(2).camera.SetFocalPoint(cam_high_random[0], center[1], cam_high_random[2])
     plt.at(2).camera.SetPosition(cam_high_random[0], - temp_pos[1], cam_high_random[2])
-    plt.at(2).camera.Zoom(2.8)    
+    plt.at(2).camera.Zoom(2.7)    
 
     plt.render()
 
@@ -412,7 +412,7 @@ plt.at(4).show(Picture(f"result_text/{position_list[order - 1]}.png"),axes=0, zo
 cam_distance = -temp_pos[1] - center[1]
 cyl_distance = box.pos()[1] - center[1]
 
-plt.at(2).camera.Zoom(2.8)    
+plt.at(2).camera.Zoom(2.7)    
 
 # Add a button to the plotter with buttonfunc as the callback function
 bu1 = plt.at(0).add_button(
